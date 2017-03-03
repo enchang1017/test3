dbd754496a8d5763e82b4d31a67ef90c6c2df8a8
enchang <enchang1017@gamil.com>
2017-03-03 11:53:21 +0800

    ass

--- try/try/main.cpp
+++ try/try/main.cpp
@@ -4,12 +4,26 @@
 using namespace System;
 bool Guess(int number)
 {
-	static int target = -1;
-	if (target == -1) {
-		Random r;
-		target = r.Next() % 100 + 1;
-	}
-	if (number == target)return true;
+    static int target = -1;
+
+    if (target == -1)
+    {
+        Random r;
+        target = r.Next() % 100 + 1;
+    }
+
+    if (number > target)
+    {
+        std::cout << "Smaller" << std::endl;
+        return false;
+    }
+    else if (number < target)
+    {
+        std::cout << "Bigger" << std::endl;
+        return false;
+    }
+
+    return true;
 }
 int main(array<System::String^>^ args)
 {
